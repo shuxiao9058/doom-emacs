e22140d6e1cae1f65819cd338792e2153ecb100c
Aaron Chi <shuxiao9058@gmail.com>
2020-02-22 18:26:40 +0800

    rime

--- modules/private/chinese/packages.el
+++ modules/private/chinese/packages.el
@@ -5,11 +5,11 @@
 
 (when (featurep! +rime)
   (package! liberime-config
-    :pin "282d37c44b"
     :recipe (
-        :local-repo "~/workspace/liberime"
-         :files ("liberime-config.el" "src" "CMakeLists.txt" "Makefile")
-                   )))
+             :host github
+             :repo "shuxiao9058/liberime"
+             :branch "feature/add_schema_config"
+             :files ("liberime-config.el" "src" "CMakeLists.txt" "Makefile"))))
 
 (package! fcitx :pin "12dc2638dd")
 (package! ace-pinyin :pin "8b2e9335b0")
