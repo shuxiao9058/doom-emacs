fe07f9aa96ea47e575e7341733a13d244ad45be8
Aaron Chi <shuxiao9058@gmail.com>
2020-02-26 11:26:23 +0800

    emacs-rime

--- modules/private/chinese/config.el
+++ modules/private/chinese/config.el
@@ -1,20 +1,19 @@
 ;;; private/chinese/config.el -*- lexical-binding: t; -*-
 
-(use-package! pyim
-  :after-call after-find-file pre-command-hook
-  :config
-  (setq pyim-dcache-directory (concat doom-cache-dir "pyim/")
-        pyim-page-tooltip t
-        pyim-debug t
-        default-input-method "pyim")
-  (when (featurep! +rime)
-    (setq pyim-default-scheme 'rime
-          pyim-rime-limit 100
-          pyim-title "ㄓ")))
+;; (use-package! pyim
+;;   :after-call after-find-file pre-command-hook
+;;   :config
+;;   (setq pyim-dcache-directory (concat doom-cache-dir "pyim/")
+;;         pyim-page-tooltip t
+;;         pyim-dcache-auto-update nil
+;;         pyim-debug t
+;;         default-input-method "pyim")
+;;   (when (featurep! +rime)
+;;     (setq pyim-default-scheme 'rime
+;;           pyim-rime-limit 100
+;;           pyim-title "ㄓ")))
 
 (use-package! liberime-config
-  :when (featurep! +rime)
-  ;; :after pyim
   :defer 1
   :init
   (setq liberime-user-data-dir (expand-file-name "local/pyim/rime" doom-private-dir))
@@ -22,28 +21,45 @@
   (add-hook 'liberime-after-start-hook
             (lambda ()
               (liberime-select-schema "guhuwubi")))
-  :config
-  (liberime-load)
+  ; :config
+  ; (liberime-load)
+  ;; (setq default-input-method "erime"
+
+  ;;       ;; erime-prompt-tooltip 'posframe
+  ;;       ;; erime-prompt-tooltip 'minibuffer
+  ;;       )
   )
 
-(use-package! pangu-spacing
-  :hook (text-mode . pangu-spacing-mode)
+(use-package! rime
+  :defer 1
+  :after liberime-config
   :config
-  ;; Always insert `real' space in org-mode.
-  (setq-hook! 'org-mode-hook pangu-spacing-real-insert-separtor t))
+  (setq default-input-method "rime"
+          ;; rime-show-candidate 'overlay
+          ;; rime-show-candidate 'posframe
+          ;; rime-show-candidate 'minibuffer
+          rime-show-candidate 'popup
+          )
+  )
+
+;; (use-package! pangu-spacing
+;;   :hook (text-mode . pangu-spacing-mode)
+;;   :config
+;;   ;; Always insert `real' space in org-mode.
+;;   (setq-hook! 'org-mode-hook pangu-spacing-real-insert-separtor t))
 
 
-(use-package! fcitx
-  :after evil
-  :config
-  (when (executable-find "fcitx-remote")
-    (fcitx-evil-turn-on)))
+;; (use-package! fcitx
+;;   :after evil
+;;   :config
+;;   (when (executable-find "fcitx-remote")
+;;     (fcitx-evil-turn-on)))
 
 
-(use-package! ace-pinyin
-  :after avy
-  :init (setq ace-pinyin-use-avy t)
-  :config (ace-pinyin-global-mode t))
+;; (use-package! ace-pinyin
+;;   :after avy
+;;   :init (setq ace-pinyin-use-avy t)
+;;   :config (ace-pinyin-global-mode t))
 
 
 ;;
--- modules/private/chinese/packages.el
+++ modules/private/chinese/packages.el
@@ -1,16 +1,34 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/chinese/packages.el
 
-(package! pyim :recipe (:local-repo "~/workspace/pyim"))
+;; (package! pyim :recipe (:local-repo "~/workspace/pyim"))
 
-(when (featurep! +rime)
-  (package! liberime-config
-    :recipe (
-             :host github
-             :repo "shuxiao9058/liberime"
-             :branch "feature/add_schema_config"
-             :files ("liberime-config.el" "src" "CMakeLists.txt" "Makefile"))))
+;; (when (featurep! +rime)
+;;   (package! liberime-config
+;;     :recipe (
+;;              :host github
+;;              :repo "shuxiao9058/liberime"
+;;              :branch "feature/add_schema_config"
+;;              :files ("liberime-config.el" "src" "CMakeLists.txt" "Makefile"))))
 
-(package! fcitx :pin "12dc2638dd")
-(package! ace-pinyin :pin "8b2e9335b0")
-(package! pangu-spacing :pin "f92898949b")
+;; (package! fcitx :pin "12dc2638dd")
+;; (package! ace-pinyin :pin "8b2e9335b0")
+;; (package! pangu-spacing :pin "f92898949b")
+
+
+(package! liberime-config
+  :recipe (
+           ;; :local-repo "~/workspace/liberime"
+           :host github
+           :repo "merrickluo/liberime"
+           ;; :repo "QiangF/liberime"
+           ;; :repo "shuxiao9058/liberime"
+           ;; :branch "feature/add_schema_config"
+           :files ("liberime-config.el" "src" "CMakeLists.txt" "Makefile")))
+
+(package! rime
+    :recipe (:host github
+        :repo "shuxiao9058/emacs-rime"
+        :files ("rime.el")
+        ))
+
